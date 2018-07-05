d3753fbe6f50c076e709ae3c22dd00b0df246b3b
mugeshnandha <34595758+mugeshnandha@users.noreply.github.com>
2018-07-05 17:06:36 +0530

    Add files via upload

---
+++ bs4c6.new.py
@@ -0,0 +1,22 @@
+n=raw_input()
+m=len(n)
+j=0
+k=m
+for i in range(m):
+    p=n[i]
+    if((p>='a' and p<='z') or (p>='A' and p<='Z')):
+        j=j+1
+    try:
+        int(p)
+    except ValueError:
+        k=k-1
+
+m=m-j
+j=n.count(' ')
+
+m=m-j
+j=n.count('.')
+m=m-j
+m=m-k
+print(m)
+    
